b6f9965c4730c55fbc8f1197be51411fa2d66729
Brock Belmonte <brockbel@gmail.com>
2018-02-13 21:59:50 -0600

    Restarted and got functioning in codepen, implemented, altered and commented.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,73 @@
-// Dog Themed Hangman
+var MAX_GUESSES = 10;
+var WORDS = ['apple', 'apricot', 'avocado', 'banana', 'blackberry', 'blueberry', 'boysenberry', 'cherry', 'cranberry', 'date', 'dragonfruit', 'elderberry', 'fig', 'grape', 'grapefruit', 'guava', 'jackfruit', 'kiwi', 'kumquat', 'lemon', 'lime', 'mango', 'watermelon', 'cantaloupe', 'honeydew', 'nectarine', 'orange', 'clementine', 'papaya', 'passionfruit', 'peach', 'pear', 'plum', 'pineapple', 'pomegranite', 'raspberry', 'strawberry'];
 
-// display "Press any key to get started!"
-// document.getElementById("startScreen").innerHTML("Press any key to get started!");
-if(onkeyup === "true")
-{
-    // document.getElementById("startScreen").innerHTML("");
-    var targetDiv = document.getElementById("startScreen");
+var started = false; 
+var currentGuesses = 0;
+var lettersGuessed = [];
+var wins = 0;
+var currentWord = '';
+var gameDisplay = [];
 
-      targetDiv.innerHTML = "";
-}
-else
-{
-    var targetDiv = document.getElementById("startScreen");
-
-        targetDiv.innerHTML = "Press any key to get started!";
-}
-// Check if the user has enough lives to play.
-// If they have enough lives then pass their choice through a loop to see if it is in the word.
-// For every instance it is in the word display a letter.
-// If the letter is not in the word then add it to the incorrect guesses and show an image on the hangman and take a life away.
-// If the entire word is guessed reset lives to 10, image to blank, incorrect guesses to blank, and use a new word. Add number of words guessed correctly and number of losses.
-
-
-
-
-
-
-/*
-
-
-// guessing all of the letters results in a win
-
-// User will pick the letter by typing it (add clicking and a virtual keyboard if time permits)
-
-// If all of the body is drawn then you lose
-
-//array of the words - add more
-var dictionary = ["German Shepherd", "Labrador Retriever", "Siberian Husky", "English Bulldog", "French Bulldog", "Poodle", "Corgie", "Alaskan Malamute", "Golden Retriever"]
-
-// someway for the user to interact with the board
-// the event listener works, I cannot get it to loop through all instances of the word yet.
-document.onkeyup = function(choice)
-{
-    console.log(choice.key);
-    currentWord.indexOf(choice.key);
-    console.log(currentWord.indexOf(choice.key));
-    return currentWord.indexOf(choice.key);
+/* This function hides the start screen and displays the game after a key is pressed.
+ This function also adds win counter, guesses left counter, letters guessed display, and
+ the underscores/gameboard. */
+function renderGame() {
+  document.getElementById('startScreen').style = 'display: none';
+  document.getElementById('game').style = 'display: block';
+  document.getElementById('winNumber').innerHTML = wins;
+  document.getElementById('guessNumber').innerHTML = MAX_GUESSES - currentGuesses;
+  document.getElementById('lettersGuessed').innerHTML = lettersGuessed.join(' ');
+  document.getElementById('gameBoard').innerHTML = gameDisplay.join(' ');
 }
 
+/* This function contains the loop that goes through and picks a word to display.
+ It picks it at random from the array and displays the correct number of underscores.
+ It emptys the letters guessed, current guesses left, and the underscores for a fresh start */
+function resetGame() {
+  var randomWordIndex = Math.floor(Math.random() * WORDS.length);
+  
+  currentGuesses = 0;
+  lettersGuessed = [];
+  gameDisplay = [];
+  currentWord = WORDS[randomWordIndex];
+  for(var i = 0; i < currentWord.length; i++) {
+    gameDisplay.push('_');
+  }
+}
 
+/* This function will run to detect when a key is pressed. It will then run "resetGame()" and "renderGame()"
+   The if statement is looking to see if the game has started or not.
+   If it has the start screen will go away and the game will be displayed.
+   If not then it will wait until a key is pressed. */
+function keyPressed(e) {
+  if(started === false) {
+    started = true;
+    resetGame();
+    renderGame();
+  } else {
+    var keyTyped = e.key;
+    var foundMatch = (currentWord.indexOf(keyTyped) >= 0);
+    if(lettersGuessed.indexOf(keyTyped) === -1) {
+      lettersGuessed.push(keyTyped);
+      if(foundMatch) {
+        for(var i = 0; i < currentWord.length; i++) {   // This will loop through the current word and replace any "_" with the character if it is correct.
+          if(currentWord[i] === keyTyped) {
+            gameDisplay[i] = keyTyped;
+          }
+        }
+        if(gameDisplay.indexOf('_') === -1) {   // This detects if there are any "_" left in the word if not you win and the game resets.
+          wins++;
+          resetGame();
+        }
+      } else {  // adds to the currentGuesses variable which will subtract from the max to display the correct variable.
+        currentGuesses++;
+        if(currentGuesses === MAX_GUESSES) {
+          resetGame();
+        }
+      }
+    }
+    renderGame();
+  }
+}
 
-// conditional logic to see if you win or lose
-var playerWins = 0;
-var playerLosses = 0;
-
-//random word generator - Works
-var rand = dictionary[Math.floor(Math.random() * dictionary.length)];
-
-var currentWord = rand;
-   console.log(currentWord)
-
-var usedWords = [];
-
-
-
-*/
+document.addEventListener('keypress', keyPressed);  // Listens for any key to be pressed
